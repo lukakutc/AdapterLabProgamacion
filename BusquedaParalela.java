d833ae28512b4ec8bd3fb9b1832a52df3b259ce7
Dany <daniel.carrasco@est.fi.uncoma.edu.ar>
2025-08-25 22:30:09 -0300

    Hice esta rama para no tocar la principal, fijense si les gusta el codigo. Se apliza el ForkJoin con RecursiveAction para buscar a una persona por su DNI. Solo falta que lo prueben

---
+++ BusquedaParalela.java
@@ -0,0 +1,47 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class BusquedaParalela extends RecursiveAction {
+//Realiza una busqueda paralela en la base de datos para encontrar una persona por su DNI
+    private static final int UMBRAL = 1000; // Umbral para dividir tareas
+    private BaseDeDatos[] datos;
+    private int inicio;
+    private int fin;
+    private int dniBuscado;
+    
+    public BusquedaParalela(int[] datos, int inicio, int fin, int dniBuscado) {
+        this.datos = datos;
+        this.inicio = inicio;
+        this.fin = fin;
+        this.dniBuscado = dniBuscado;
+    }
+    
+    @Override
+    protected void compute() {
+        if (fin - inicio <= UMBRAL) {
+            buscarSecuencialmente();
+        } else {
+            int medio = (inicio + fin) / 2;
+            
+            BusquedaParalela izquierda = new BusquedaParalela(datos, inicio, medio, dniBuscado);
+            BusquedaParalela derecha = new BusquedaParalela(datos, medio, fin, dniBuscado);
+            
+            // Ejecuta las subtareas en paralelo
+            invokeAll(izquierda, derecha);
+        }
+    }
+    
+    private void buscarSecuencialmente() {
+        // Realiza la búsqueda secuencialmente en el rango dado
+        boolean encontrado = false;
+        for (int i = inicio; i < fin; i++) {
+            if (datos[i].getDni().equals(dniBuscado)) {
+                encontrado = true;
+                System.out.println("Se encuentra persona con el dni" + dniBuscado);
+            }
+        }
+        if (!encontrado) {
+            System.out.println("No se encuentra persona con el dni" + dniBuscado);
+        }
+    }
+}
